058bd0713fc6526492099a58c31e6e86dfc34870
atilla525 <52794740+atilla525@users.noreply.github.com>
2021-07-01 14:37:08 +0300

    lel

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = "root/static"
+STATIC_ROOT = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
